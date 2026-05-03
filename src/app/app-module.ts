7c21fa1bf0f96594f2be12bc95f8cb1b72d3e23c
artartur <arturgfs@hotmail.com>
2026-05-03 00:48:29 -0300

    chore: initial settings

---
+++ src/app/app-module.ts
@@ -0,0 +1,13 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay())],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

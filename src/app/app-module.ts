c6ac42553c715ae711a416f69cb238f10f69ff8d
artartur <arturgfs@hotmail.com>
2026-05-03 02:15:15 -0300

    feat(login): create login/home screen

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,11 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing-module';
 import { AppComponent } from './app.component';
+import { LoginModule } from '../login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, LoginModule],
   providers: [provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay())],
   bootstrap: [AppComponent],
 })
